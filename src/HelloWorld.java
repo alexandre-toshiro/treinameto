5467cbfe6c675cd63af6451e4954d522a884622f
Alexandre Toshiro <atoshiro@indracompany.com>
2020-02-17 15:58:47 -0300

    Aleatório

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World - Alexandre Toshiro, 24 anos.");
+		System.out.println("Hello World - Alexandre Toshiro, 24 anos.ok");
 
 	}
 
